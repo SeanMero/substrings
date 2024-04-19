3e7d311b65e4162dc7df2f611191a62db23e771d
SeanMero <MeroFromVero@gmail.com>
2024-04-19 17:57:35 +0800

    Remove bugchecking

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,8 @@
-require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(phrase, subs)
   phrase_array = phrase.downcase.split(" ")
   subs.reduce(Hash.new(0)) do |acc, sub|
-   # binding.pry
     phrase_array.each do |word|
       if word.include?(sub) == true
       acc[sub] += 1
@@ -13,5 +11,3 @@ def substrings(phrase, subs)
     acc
   end
 end
-
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
